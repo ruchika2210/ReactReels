1879fdb3f8e72acdde948dc0370dfa9f6ee72760
Ruchika <ruchika.3brain@gmail.com>
2023-07-16 00:48:36 +0530

    Context API implmenttation start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import react, { useState } from "react";
-import logo from "./logo.svg";
+import react, { Children, useState } from "react";
+import {  Routes, Route, BrowserRouter,Navigate   } from 'react-router-dom'
+
 import "./App.css";
 import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Feed from "./Components/Feed";
+import {AuthProvider} from "./contextContainer/AuthContext"
+let isSignedUp=Math.random()>0.5?true:false;
 
 function App() {
   return (
     <>
-      <Login />
+    <BrowserRouter>
+    <AuthProvider>    
+        <Routes>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/" exact element={<PrivateRoute><Feed/></PrivateRoute>} />
+
+      </Routes>
+      </AuthProvider>
+
+      </BrowserRouter>
     </>
   );
+}
+
+
+function PrivateRoute({children}) {
+
+  return isSignedUp===false ?<>{children} </> : <Navigate to="/login" />;
 }
 
 export default App;
